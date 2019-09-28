8b6c33d1214260a6f9718eaeee54298cdb3286da
Ammar <ammarkhaliq@gmail.com>
2019-09-28 21:16:47 +0500

    First Commit

---
+++ kdd99_sm_sgd.py
@@ -0,0 +1,90 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Activation
+from keras.optimizers import SGD
+from mlxtend.preprocessing import one_hot
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv('/content/drive/My Drive/10 per/kddcup.data_10_percent_corrected')
+dataset.drop_duplicates(inplace=True)
+
+X = dataset.iloc[:, :-1].values     #selects all col except last
+y = dataset.iloc[:, -1].values      #selects last col only
+
+
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+Port_encoder = LabelEncoder()
+X[:, 1 ] = Port_encoder.fit_transform(X[:, 1])
+
+Protocol_encoder = LabelEncoder()
+X[:, 2 ] = Protocol_encoder.fit_transform(X[: , 2])
+
+Flag_encoder = LabelEncoder()
+X[:, 3] =Flag_encoder.fit_transform(X[: , 3])
+
+onehotencoder = OneHotEncoder(categorical_features=[1,2,3])
+X = onehotencoder.fit_transform(X).toarray()
+
+
+output_encoder = LabelEncoder()
+y =output_encoder.fit_transform(y)
+
+#onehotencoder2 = OneHotEncoder()
+y=one_hot(y)        #mlxtend lib one_hots the 1d array
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+input_dim=X_train.shape[1]
+no_of_classes = y_train.shape[1]
+
+
+#adding layers
+model = Sequential()
+model.add(Dense(64,activation='sigmoid',input_dim=input_dim))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+model.add(Dropout(0.8))
+model.add(Dense(64,activation='sigmoid'))
+#model.add(Dropout(0.8))
+
+model.add(Dense(no_of_classes,activation='softmax'))
+
+#stochastic_gradient_descent
+sgd=SGD(lr=0.05,decay=1e-6,momentum=0.9,nesterov=True)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy']
+             )
+
+model.fit(X_train,y_train,
+          epochs=100,
+          batch_size=200)
+
+
+score = model.evaluate(X_test, y_test, batch_size=200)
+
+print(score[1])
